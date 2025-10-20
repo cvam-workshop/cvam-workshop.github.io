941960692226325d27db04450f1f539ca40f7652
Mac Szankin <maciej.szankin@sima.ai>
2025-10-20 05:38:39 -1000

    Add display links to schedule

--- _src/main.js
+++ _src/main.js
@@ -13,3 +13,23 @@ $("[data-off-canvas-wrapper]")
   .bind("opened.zf.offcanvas closed.zf.offcanvas", function(e) {
     $(".hamburger").toggleClass("is-active");
 });
+
+// Check URL parameter and show/hide slides links
+$(document).ready(function() {
+  function checkSlidesParam() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const showSlides = urlParams.get('show-slides') === '1';
+    
+    if (showSlides) {
+      $('.slides-link-container').addClass('show-slides');
+    } else {
+      $('.slides-link-container').removeClass('show-slides');
+    }
+  }
+  
+  // Check on page load
+  checkSlidesParam();
+  
+  // Also check when URL changes (for single page applications or back/forward navigation)
+  $(window).on('popstate', checkSlidesParam);
+});
